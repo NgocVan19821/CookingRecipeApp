dbbd9e6e1cd15996ea28c3508a076c412365e54f
NgocVan19821 <ngocvan19821@gmail.com>
2022-09-25 22:55:03 +0700

    update

--- app/src/main/java/com/example/cookingrecipeapp/feature/home/HomeActivity.kt
+++ app/src/main/java/com/example/cookingrecipeapp/feature/home/HomeActivity.kt
@@ -1,12 +1,19 @@
 package com.example.cookingrecipeapp.feature.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cookingrecipeapp.R
+import com.example.cookingrecipeapp.feature.detail.DetailActivity
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        item.setOnClickListener {
+            var i = Intent(this, DetailActivity::class.java)
+            startActivity(i)
     }
-}
+}}
